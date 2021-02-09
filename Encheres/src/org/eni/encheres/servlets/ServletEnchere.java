2a99b5c5e1c65d2701385d57c72098987c89de1c
eondo <eondo@ELODIE-HP>
2021-02-09 16:18:06 +0100

    servletEncheres

---
+++ Encheres/src/org/eni/encheres/servlets/ServletEnchere.java
@@ -0,0 +1,32 @@
+package org.eni.encheres.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletEnchere
+ */
+@WebServlet("/ServletEnchere")
+public class ServletEnchere extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// renvoi vers liste des enchères
+		getServletContext().getRequestDispatcher("/accueil").forward(request, response);
+	}
+
+}
