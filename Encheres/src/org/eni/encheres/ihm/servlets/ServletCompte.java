975a4d5c5bcb17fba952a3b900ed51cc55a58218
TaharqaRuffel <76183417+TaharqaRuffel@users.noreply.github.com>
2021-02-11 13:58:10 +0100

    s'inscrire IHM

--- Encheres/src/org/eni/encheres/ihm/servlets/ServletCompte.java
+++ Encheres/src/org/eni/encheres/ihm/servlets/ServletCompte.java
@@ -17,11 +17,21 @@ import org.eni.encheres.ihm.exceptions.MotDePasseException;
  */
 @WebServlet("/ServletCompte")
 public class ServletCompte extends HttpServlet {
+
+	private static final String ATTR_ERREUR_INSERTION = "erreurInsertion";
+	private static final String ATTR_ERREUR_IDENTIFIANT = "erreurIdentifiant";
+	private static final String ATTR_ERREUR_MDP = "erreurMdp";
+	
 	private static final String ERREUR_GENERIQUE = "generique";
 	private static final String ERREUR_IDENTIFIANT = "identifiant";
 	private static final String ERREUR_MDP = "mdp";
+
 	private static final String ATTR_ERREUR_MESSAGE = "ErreurMessage";
-	private static final String SERVLET_CONNEXION = "/ServletConnexion";
+
+	private static final String SERVLET_CONNEXION = "/ServletConnexion"
+	
+	private static final String JSP_COMPTE = "/compte";
+	
 	private static final String PARAM_CREATION = "creation";
 	private static final String PARAM_VILLE = "ville";
 	private static final String PARAM_CODE_POSTAL = "codePostal";
@@ -31,15 +41,12 @@ public class ServletCompte extends HttpServlet {
 	private static final String PARAM_PRENOM = "prenom";
 	private static final String PARAM_NOM = "nom";
 	private static final String PARAM_PSEUDO = "pseudo";
-	private static final String JSP_COMPTE = "/compte";
 	private static final String PARAM_CONFIRMATION = "confirmation";
 	private static final String PARAM_MDP = ERREUR_MDP;
 	private static final String PARAM_SUPPRESSION = "suppression";
 	private static final String PARAM_MODIFICATION = "modification";
 	private static final long serialVersionUID = 1L;
 
-	
-	
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
@@ -50,14 +57,14 @@ public class ServletCompte extends HttpServlet {
 		Boolean modification;
 		Boolean suppression;
 		response.setCharacterEncoding("UTF-8");
-		
+
 		creation = Boolean.parseBoolean(request.getParameter(PARAM_CREATION));
 		modification = Boolean.parseBoolean(request.getParameter(PARAM_MODIFICATION));
 		suppression = Boolean.parseBoolean(request.getParameter(PARAM_SUPPRESSION));
 
 		if (modification != null && modification) {
-			if(!creation) {
-				
+			if (!creation) {
+
 			}
 			getServletContext().getRequestDispatcher(JSP_COMPTE).forward(request, response);
 
@@ -71,8 +78,10 @@ public class ServletCompte extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		boolean ErreurSaisie = false;
 		response.setCharacterEncoding("UTF-8");
-		
+
 		String pseudo = request.getParameter(PARAM_PSEUDO);
 		String nom = request.getParameter(PARAM_NOM);
 		String prenom = request.getParameter(PARAM_PRENOM);
@@ -81,38 +90,71 @@ public class ServletCompte extends HttpServlet {
 		String rue = request.getParameter(PARAM_RUE);
 		String codePostal = request.getParameter(PARAM_CODE_POSTAL);
 		String ville = request.getParameter(PARAM_VILLE);
-		String mdp = request.getParameter(PARAM_MDP) ;
+		String mdp = request.getParameter(PARAM_MDP);
 		String confirmation = request.getParameter(PARAM_CONFIRMATION);
 		UtilisateurManager manager = UtilisateurManager.getInstance();
-		
+
+		// try {
+		// controlerMdp(mdp,confirmation);
+		// } catch (MotDePasseException mdpe) {
+		// request.setAttribute(ATTR_ERREUR_MESSAGE, ERREUR_MDP);
+		// getServletContext().getRequestDispatcher(JSP_COMPTE).forward(request,
+		// response);
+		// }
+		//
+		// try {
+		// manager.controleIdentifiantNewUtilisateur(pseudo,email);
+		// } catch (BLLException e) {
+		// request.setAttribute(ATTR_ERREUR_MESSAGE, ERREUR_IDENTIFIANT);
+		// getServletContext().getRequestDispatcher(JSP_COMPTE).forward(request,
+		// response);
+		// }
+		//
+		// try {
+		// manager.ajouterUtilisateur(pseudo, nom, prenom, email, telephone, rue,
+		// codePostal, ville, mdp);
+		// }catch(BLLException blle) {
+		// request.setAttribute(ATTR_ERREUR_MESSAGE, ERREUR_GENERIQUE);
+		// blle.printStackTrace();
+		// getServletContext().getRequestDispatcher(JSP_COMPTE).forward(request,
+		// response);
+		// }
+
 		try {
-			controlerMdp(mdp,confirmation);
+			controlerMdp(mdp, confirmation);
+			request.setAttribute(ATTR_ERREUR_MDP, false);
 		} catch (MotDePasseException mdpe) {
-			request.setAttribute(ATTR_ERREUR_MESSAGE, ERREUR_MDP);
-			getServletContext().getRequestDispatcher(JSP_COMPTE).forward(request, response);
+			request.setAttribute(ATTR_ERREUR_MDP, true);
+			ErreurSaisie=true;
 		}
-		
+
 		try {
-			manager.controleIdentifiantNewUtilisateur(pseudo,email);
+			manager.controleIdentifiantNewUtilisateur(pseudo, email);
+			request.setAttribute(ATTR_ERREUR_IDENTIFIANT, false);
 		} catch (BLLException e) {
-			request.setAttribute(ATTR_ERREUR_MESSAGE, ERREUR_IDENTIFIANT);
-			getServletContext().getRequestDispatcher(JSP_COMPTE).forward(request, response);
+			request.setAttribute(ATTR_ERREUR_IDENTIFIANT, true);
+			ErreurSaisie=true;
 		}
-		
+
 		try {
 			manager.ajouterUtilisateur(pseudo, nom, prenom, email, telephone, rue, codePostal, ville, mdp);
-		}catch(BLLException blle) {
-			request.setAttribute(ATTR_ERREUR_MESSAGE, ERREUR_GENERIQUE);
+			request.setAttribute(ATTR_ERREUR_INSERTION, false);
+		} catch (BLLException blle) {
+			request.setAttribute(ATTR_ERREUR_INSERTION, true);
 			blle.printStackTrace();
-			getServletContext().getRequestDispatcher(JSP_COMPTE).forward(request, response);
+			ErreurSaisie=true;
 		}
 		
-		getServletContext().getRequestDispatcher(SERVLET_CONNEXION).forward(request, response);
+		if (ErreurSaisie) {
+			getServletContext().getRequestDispatcher(JSP_COMPTE).forward(request, response);
+		} else {
+			getServletContext().getRequestDispatcher(SERVLET_CONNEXION).forward(request, response);
+		}
 
 	}
 
 	private void controlerMdp(String mdp, String confirmation) throws MotDePasseException {
-		if(!mdp.equals(confirmation) ) {
+		if (!mdp.equals(confirmation)) {
 			throw new MotDePasseException("La confirmation du mot de passe est différente du mot de passe");
 		}
 	}
